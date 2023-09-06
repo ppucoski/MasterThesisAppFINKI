6745d78cad787b511ed4bcb389f8254cb3a70cd7
Stefan Risteski <stefanristeski2001@gmail.com>
2023-09-06 04:10:28 +0200

    Additional Repository and Service Changes

--- src/main/java/vp/magisterski/service/impl/StudentServiceImpl.java
+++ src/main/java/vp/magisterski/service/impl/StudentServiceImpl.java
@@ -2,14 +2,21 @@ package vp.magisterski.service.impl;
 
 import org.springframework.stereotype.Service;
 import vp.magisterski.model.shared.Student;
+import vp.magisterski.repository.StudentRepository;
 import vp.magisterski.service.StudentService;
 
 import java.util.Optional;
 
 @Service
 public class StudentServiceImpl implements StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @Override
-    public Optional<Student> findStudent(String student) {
-        return Optional.empty();
+    public Optional<Student> findStudentByIndex(String index) {
+        return this.studentRepository.findByIndex(index);
     }
 }
