2319b558dcccd3523b4c1f4ab6e4da162d2ac2f9
Stefan Risteski <stefanristeski2001@gmail.com>
2023-09-07 12:53:44 +0200

    small changes

--- src/main/java/vp/magisterski/model/magister/MasterThesisStatus.java
+++ src/main/java/vp/magisterski/model/magister/MasterThesisStatus.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum MasterThesisStatus {
-    //TODO: en_us i prashalnicite nekako da isceznat lang=mk mozda vo html neshto.
     PROFESSOR_THESIS_REGISTRATION(1, "Професорот ја регистрира тезата."),
     STUDENT_APPROVAL(2, "Студентот ја прифаќа тезата."),
     ADMINISTRATION_VALIDATION(3, "Валидација од страна на администрација."),
@@ -18,8 +17,8 @@ public enum MasterThesisStatus {
     MENTOR_PRESENTATION_SCHEDULING(9, "Менторот ја закажува презентацијата."),
     PENDING_GRADE(10, "Очекување на оцена од менторот."),
     MEMBERS_GRADE_APPROVAL(11, "Членовите на комисијата ја одобруваат оцената."),
-    ARCHIVE(12, "Архива"),
-    CANCELED(30, "Откажано");
+    ARCHIVE(12, "Архива."),
+    CANCELED(30, "Откажано.");
 
     private final double order;
     private final String displayName;
