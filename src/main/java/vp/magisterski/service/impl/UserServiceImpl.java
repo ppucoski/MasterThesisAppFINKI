2f39a485bef11668898322a84b05c8b83114634f
201071 <pavel.sprostran@students.finki.ukim.mk>
2024-02-25 14:01:45 +0100

    Added username tracking

--- src/main/java/vp/magisterski/service/impl/UserServiceImpl.java
+++ src/main/java/vp/magisterski/service/impl/UserServiceImpl.java
@@ -1,6 +1,11 @@
 package vp.magisterski.service.impl;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import vp.magisterski.config.FacultyUserDetails;
 import vp.magisterski.model.shared.User;
 import vp.magisterski.repository.UserRepository;
 import vp.magisterski.service.UserService;
@@ -18,5 +23,19 @@ public class UserServiceImpl implements UserService {
     @Override
     public Optional<User> findUserById(String id) {
         return this.userRepository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public String getUsernameFromUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated() && !authentication.getPrincipal().equals("anonymousUser")) {
+            String username = authentication.getName();
+            User user = userRepository.findById(username).orElse(null);
+            if (user != null) {
+                return user.getName();
+            }
+        }
+        return null;
     }
 }
--- src/main/java/vp/magisterski/web/HomeController.java
+++ src/main/java/vp/magisterski/web/HomeController.java
@@ -3,24 +3,39 @@ package vp.magisterski.web;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import vp.magisterski.config.FacultyUserDetails;
+import vp.magisterski.model.shared.User;
 import vp.magisterski.service.MasterThesisService;
+import vp.magisterski.service.UserService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping({"/",  "/index"})
 public class HomeController {
     private final MasterThesisService masterThesisService;
+    private final UserService userService;
 
-    public HomeController(MasterThesisService masterThesisService) {
+    public HomeController(MasterThesisService masterThesisService, UserService userService) {
         this.masterThesisService = masterThesisService;
+        this.userService = userService;
     }
 
+    public void user(Model model){
+        String username = userService.getUsernameFromUser();
+        model.addAttribute("user", username);
+    }
+
+
     @GetMapping
-    public String getHomePage(){
+    public String getHomePage(Model model){
+        user(model);
         return "index";
     }
 
@@ -28,6 +43,7 @@ public class HomeController {
     public String findAllThesis(@RequestParam(defaultValue = "0") int page,
                                 @RequestParam(defaultValue = "3") int size,
                                 Model model) {
+        user(model);
         Pageable pageable = PageRequest.of(page, size, Sort.by("id"));
         int total = masterThesisService.findAll().size();
         model.addAttribute("MasterThesis", masterThesisService.findAll(pageable));
