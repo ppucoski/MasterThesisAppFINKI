dddcbc0ec0a1e59d4b6650bd3f90e3801e9952aa
201187 <petar.pucoski@students.finki.ukim.mk>
2024-02-25 02:28:58 +0100

    Slight change to authentication

--- src/main/java/vp/magisterski/config/AuthConfig.java
+++ src/main/java/vp/magisterski/config/AuthConfig.java
@@ -1,5 +1,8 @@
 package vp.magisterski.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import vp.magisterski.model.enumerations.AppRole;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,35 +11,22 @@ import org.springframework.security.config.annotation.web.configurers.LogoutConf
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
 public class AuthConfig {
 
-    public HttpSecurity authorize(HttpSecurity http) throws Exception {
-        return http
+    @Bean
+    public SecurityFilterChain authorize(HttpSecurity http) throws Exception {
+        http.formLogin(withDefaults())
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
-                        .requestMatchers("/admin/*").hasAnyRole(
-                                AppRole.PROFESSOR.name(),
-                                AppRole.ADMIN.name()
-                        )
-//                        .requestMatchers("/admin/subject-requests/{professorId}/**").access(
-//                                new WebExpressionAuthorizationManager("#professorId == authentication.name or hasRole('ROLE_ADMIN')")
-//                        )
-//                        .requestMatchers("/engagement/{professorId}/**").access(
-//                                new WebExpressionAuthorizationManager("#professorId == authentication.name or hasRole('ROLE_ADMIN')")
-//                        )
-//                        .requestMatchers("/admin/**", "/api/**", "/build/**", "/engagement/init").hasAnyRole(
-//                                AppRole.ADMIN.name()
-//                        )
-//                        .requestMatchers("/active-subjects", "io.png", "/allocation/*").permitAll()
-                        .anyRequest().permitAll() //testing purposes
-//                        .anyRequest().authenticated() //ACTUAL
-                )
-                .logout((logout) -> logout
-                        .logoutUrl("/logout")
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .logoutSuccessUrl("/")
-                        .permitAll());
-//                .logout(LogoutConfigurer::permitAll);
+                                .requestMatchers("/admin/**").hasAnyRole(
+                                        AppRole.PROFESSOR.name(),
+                                        AppRole.ADMIN.name()
+                                )
+                                .anyRequest().permitAll()
+                );
+        return http.build();
     }
 }
